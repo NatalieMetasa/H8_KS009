863e3be761ed3d843c30549f138d32f2efc98cad
NatalieMetasa <natalienovenrodumetasa@gmail.com>
2022-09-05 14:49:37 +0800

    add sesi 2 and gitignore

---
+++ sesi 3/mod.py
@@ -0,0 +1,12 @@
+s = "Hacktiv8-PTP Python For Data Science"
+a = [100, 200, 300]
+
+def foo(arg):
+    print(f'arg = {arg}')
+  #  print('arg = ', arg) #artinya sama dengan  print(f'arg = {arg}') 
+    
+    class Foo:
+        pass
+    
+    print(s)
+    print(a)
